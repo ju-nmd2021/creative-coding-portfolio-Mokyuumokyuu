08164aadee4dd97ce1175bab4e9abb9fc38ff6f2
Mokyuumokyuu <93583916+Mokyuumokyuu@users.noreply.github.com>
2023-08-31 11:37:13 +0200

    Updated Code
    just changed color for beads

--- experiments/thing.js
+++ experiments/thing.js
@@ -1,29 +1,31 @@
+//Variables
 function setup() {
   createCanvas(innerWidth, innerHeight);
   background(255, 255, 255);
   noLoop();
-  //colorMode(HSB, 100);
 }
 
+//drawing beads
 function draw() {
   background(255, 255, 255);
-  for (let x = 5; x < 300; x = x + 13) {
+  for (let x = 5; x < 300; x = x + 14) {
     for (let y = 5; y < 300; y = y + 25) {
       if (x < 200 && x > 100) {
         strokeWeight(0.4);
         fill(255, 255, 255);
-        rect(x + 24, y + 95, 15, 15);
+        rect(x + 64, y + 94, 12, 12);
       } else {
         push();
         noStroke();
-        fill(random(0, 400), random(0, 100), 70);
-        ellipse(x + 30, y + 100, 13);
+        fill(random(0, 60), random(0, 200), 70);
+        ellipse(x + 70, y + 100, 14);
         pop();
       }
     }
   }
 }
 
+//changes colors on click
 function mouseClicked() {
   draw();
 }
