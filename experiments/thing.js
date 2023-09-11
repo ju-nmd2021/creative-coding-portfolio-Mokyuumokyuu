f549c0780c76437cd9bc30e350d07bcaab086089
Mokyuumokyuu <93583916+Mokyuumokyuu@users.noreply.github.com>
2023-09-11 09:33:36 +0200

    Added const variables

--- experiments/thing.js
+++ experiments/thing.js
@@ -5,11 +5,15 @@ function setup() {
   noLoop();
 }
 
+const size = 15;
+const gap = 20;
+const ammount = 46;
+
 //drawing beads
 function draw() {
   background(255, 255, 255);
   for (let x = 5; x < 300; x = x + 14) {
-    for (let y = 5; y < 300; y = y + 25) {
+    for (let y = 5; y < 150; y = y + 25) {
       if (x < 200 && x > 100) {
         strokeWeight(0.4);
         fill(255, 255, 255);
@@ -17,7 +21,7 @@ function draw() {
       } else {
         push();
         noStroke();
-        fill(random(0, 60), random(0, 200), 70);
+        fill(random(0, 250), random(0, 300), 255);
         ellipse(x + 70, y + 100, 14);
         pop();
       }
